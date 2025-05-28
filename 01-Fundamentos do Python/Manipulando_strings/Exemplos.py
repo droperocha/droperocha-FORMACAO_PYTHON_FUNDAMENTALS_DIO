15cf619f42dbc46e07082efe5870370d1b479901
droperocha <pedro2846@hotmail.com>
2025-05-28 10:35:11 -0300

    exemplos de interpolação de variaveis em aula

--- 01-Fundamentos do Python/Manipulando_strings/Exemplos.py
+++ 01-Fundamentos do Python/Manipulando_strings/Exemplos.py
@@ -1,3 +1,4 @@
+'''
 curso ="pYtHon"
 
 print(curso.upper())
@@ -27,3 +28,52 @@ print(curso.center(20, "="))#Primeiro argumento diz quantos caracters vai ter na
 
 print(".".join(curso))
 #Parecido com o for ele junta a instrução a cada letra da variavel string
+
+'''
+#interpolação de variaveis
+
+# metodo oldstyle NÃO RECOMENDADO
+
+'''
+nome = "Pedro"
+idade = 27
+profissao = "Programador"
+linguagem = "Python"
+
+print("Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e estou matriculado no curso de %s." %(nome,idade,profissao,linguagem))
+# %s string 
+# %d inteiro
+# %f ponto flutuante 
+
+'''
+
+# metodo format
+'''
+nome = "Pedro"
+idade = 27
+profissao = "Programador"
+linguagem = "Python"
+
+print("Olá, me chamo {}. Eu tenho {} anos de idade, trabalho como {} e estou matriculado no curso de {}." .format(nome,idade,profissao,linguagem))#Manteve o mesmo dilema e problema apresentado no jeito old style em códigos ou frases grandes esse estilo fica dificil de compreender
+
+print("Olá, me chamo {3}. Eu tenho {2} anos de idade, trabalho como {0} e estou matriculado no curso de {1}." .format(nome,idade,profissao,linguagem))#errado de propósito
+
+print("Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}." .format(nome=nome,idade=idade,profissao=profissao,linguagem=linguagem))#Olha como ficou muito mais fácil
+
+# print("Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}." .format(**pessoa))#Olha como ficou muito mais fácil
+
+'''
+
+# metodo f-string
+nome = "Pedro"
+idade = 27
+profissao = "Programador"
+linguagem = "Python"
+
+print(F"Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}.")#melhor que o format mais fácil não precisa passar a sequencia de variaveis no final.
+
+PI = 3.14159265358979323846
+
+print(f"Valor de PI: {PI: .2f}")#2f numero de casas que voce quer da variavel
+print(f"Valor de PI: {PI: 10.2f}") #10 é o numero de casas para adicionar antes do resultado
+
