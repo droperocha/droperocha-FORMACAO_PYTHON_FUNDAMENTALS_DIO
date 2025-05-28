16d19de10bfece265259cc011bd1980ffc361a94
droperocha <pedro2846@hotmail.com>
2025-05-28 10:58:36 -0300

    tecnica de fatiamento em python

---
+++ 01-Fundamentos do Python/Manipulando_strings/string_3.py
@@ -0,0 +1,16 @@
+nome= "Pedro Rocha da Silva"
+
+print(nome[0])#retorna o primeiro caracter
+
+print(nome[:5])#retorna as 5 primeiras caracteres 5-1
+
+print(nome[6:])# vai começar a pegar a partir do 6
+
+print(nome[6:11])#retorna o que está entre o inicio e o fim
+
+print(nome[6:11:2])#step ele conta dentro dessa string a cada 2 passos nesse caso
+
+print(nome[:])#retorna a string inteira
+
+print(nome[::-1])#retorna a string invertida
+
